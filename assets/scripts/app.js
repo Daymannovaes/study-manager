4e01a6f536b66586898217ac74f5dbbc67f3a840
Daymannovaes <daymannovaes@gmail.com>
2014-07-29 22:00:12 -0300

    fixing toggle actions for touch

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,13 @@
 var SPapp = angular.module("screenplay", []);
+
+$info = {};
+$info.toggle = function(button) {
+	$(button).parents(".container-actions").find(".actions").stop(true, true).toggle();			
+}
+$info.show = function(button) {
+	$(button).parents(".container-actions").find(".actions").stop(true, true).delay(300).show(100);
+	
+}
+$info.hide = function(button) {
+	$(button).parents(".container-actions").find(".actions").stop(true, true).delay(300).hide(200);
+}
